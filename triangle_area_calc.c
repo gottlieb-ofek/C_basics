e076325ac9fe9e3aa218aabd8a19752be330a21f
gottl <gottlieb.ofek@gmail.com>
2025-06-18 13:44:32 +0300

    finished fundamental - traingle area

---
+++ triangle_area_calc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int base;
+    int height;
+
+
+    printf("Enter base of the triangle: ");
+    scanf("%d", &base);
+
+    printf("Enter height of the triangle: ");
+    scanf("%d", &height);
+
+    printf("yys");
+    printf("Area of the triangle = %.2f sq. units", (float)height*base/2);
+
+    return 0; 
+}
