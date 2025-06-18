6660298ae31ceca87e5529591756bb0ef51b13d8
gottl <gottlieb.ofek@gmail.com>
2025-06-18 13:28:25 +0300

    finished fundamentals - Perform arithmetic operations

--- pao.c
+++ pao.c
@@ -1,5 +1,21 @@
+#include <stdio.h>
 
 int main() {
-    printf("hi");
+    
+    int n1;
+    int n2;
+
+    printf("First number: ");
+    scanf("%d", &n1);
+
+    printf("Second number: ");
+    scanf("%d", &n2);
+
+    printf("Sum = %d\n", n1+n2);
+    printf("Difference = %d\n", n1-n2);
+    printf("Product = %d\n", n1*n2);
+    // we can have devision by zero but exceptions is not part of the basic exercise 
+    printf("Quotient = %d\n", n1/n2);
+    printf("Modulus = %d", n1%n2);
+   
     return 0;
-}
