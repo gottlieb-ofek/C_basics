cc8f9a65a652d4d0fbe2b97f2579dc40c5d5a972
gottl <gottlieb.ofek@gmail.com>
2025-06-18 20:42:08 +0300

    initial struct for toggle nth

---
+++ toggle_nth.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int num = 0;
+    int nth_bit  = 0;
+
+
+    printf("Input number: ");
+    scanf("%d", &num);
+
+    printf("Input nth bit to toggle: ");
+    scanf("%d", &nth_bit);
+
+    int num_after_toggle = 0;
+
+    printf("After toggling nth bit: %d", num_after_toggle);
+    
+
+    return 0; 
+}
