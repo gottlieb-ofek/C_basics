ccab514a28647644438e124ee58346441cde64ff
gottl <gottlieb.ofek@gmail.com>
2025-06-19 15:25:24 +0300

    finished check leap yesr

---
+++ check_leap_year.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+enum return_value_e
+{
+    UNINITIALIZED = -1, 
+    SUCCESS = 1,
+};
+
+int main() {
+    enum return_value_e error_code = UNINITIALIZED;
+    int year  = 0;
+
+    printf("Input year: ");
+    scanf("%d", &year);
+
+    if (year % 4 == 0) {
+        printf("Yay we have leap year 🤰🏻");
+        goto Exit;
+    }
+
+    printf("Nah just a regular year... ");
+
+Exit:
+    error_code = SUCCESS;
+    return SUCCESS;
+}
