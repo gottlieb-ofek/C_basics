6b525eb5e938cbb37dd2fcf358773bf4aa8091d2
gottl <gottlieb.ofek@gmail.com>
2025-06-19 13:38:31 +0300

    fixed spaces between variables

--- pao.c
+++ pao.c
@@ -11,12 +11,12 @@ int main() {
     printf("Second number: ");
     scanf("%d", &n2);
 
-    printf("Sum = %d\n", n1+n2);
-    printf("Difference = %d\n", n1-n2);
-    printf("Product = %d\n", n1*n2);
+    printf("Sum = %d\n", n1 + n2);
+    printf("Difference = %d\n", n1 - n2);
+    printf("Product = %d\n", n1 * n2);
     // we can have devision by zero but exceptions is not part of the basic exercise 
-    printf("Quotient = %f\n", (float)n1/n2);
-    printf("Modulus = %d", n1%n2);
+    printf("Quotient = %f\n", (float)n1 / n2);
+    printf("Modulus = %d", n1 % n2);
    
     return 0;
 }
