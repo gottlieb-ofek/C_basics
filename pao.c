7eca5cdd00d4dbdba77c0ba73191cafea2234adb
gottl <gottlieb.ofek@gmail.com>
2025-06-19 13:42:40 +0300

    devision by zero handling added

--- pao.c
+++ pao.c
@@ -14,9 +14,13 @@ int main() {
     printf("Sum = %d\n", n1 + n2);
     printf("Difference = %d\n", n1 - n2);
     printf("Product = %d\n", n1 * n2);
-    // we can have devision by zero but exceptions is not part of the basic exercise 
+    if (n2 == 0) {
+        printf("Error: devision by zero");
+        goto Exit;
+    }
     printf("Quotient = %f\n", (float)n1 / n2);
     printf("Modulus = %d", n1 % n2);
    
+Exit:
     return 0;
 }
