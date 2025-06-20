91e65d1e140b4449d120200af3cfc8944001da0d
gottl <gottlieb.ofek@gmail.com>
2025-06-20 11:54:12 +0300

    moved to the begining

--- toggle_nth.c
+++ toggle_nth.c
@@ -4,14 +4,13 @@ int main() {
 
     int num = 0;
     int nth_bit  = 0;
+    int num_after_toggle = 0;
 
     printf("Input number: ");
     scanf("%d", &num);
 
     printf("Input nth bit to toggle: ");
     scanf("%d", &nth_bit);
-
-    int num_after_toggle = 0;
     
     num_after_toggle = num ^ (1 << (nth_bit - 1) );
 
