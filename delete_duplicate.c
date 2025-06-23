8cdd34a7d8bec6c6a8e282623dd3a2cca8c47bca
gottl <gottlieb.ofek@gmail.com>
2025-06-23 20:10:49 +0300

    splitted into functions

--- delete_duplicate.c
+++ delete_duplicate.c
@@ -11,15 +11,16 @@ enum return_value_e
     ILLEGAL_INPUT = 1,
 };
 
+void print_array(int array[], int array_size);
 bool is_element_in_array(int array[], int element, int array_size);
+construct_new_array_with_no_duplicates(int array[], int array_size, int no_duplicates_array[]);
 
 int main() {
     enum return_value_e error_code = UNINITIALIZED;
 
     int array_size = 0;
     int current_recieved_element = 0;
-    int array_index = 0;
-    int current_element_to_inspect_duplicates = 0;
+    int no_duplicates_array_size = 0;
     int array[ARRAY_MAX_SIZE];
     int no_duplicates_array[ARRAY_MAX_SIZE];
 
@@ -42,32 +43,22 @@ int main() {
         array[i] = current_recieved_element;
     }
 
-    // now we remove duplicates
-    for(int i = 0; i < array_size; i++)
-    {
-        current_element_to_inspect_duplicates = array[i];
-        if(is_element_in_array(no_duplicates_array, current_element_to_inspect_duplicates, array_index))
-        {
-            continue;
-        }
-        no_duplicates_array[array_index] = current_element_to_inspect_duplicates;
-        array_index += 1;
-    }
-
-    for(int i = 0; i < array_size; i++)
-    {
-        printf("%d ", array[i]);
-    }
+    no_duplicates_array_size = construct_new_array_with_no_duplicates(array, array_size, no_duplicates_array);
+    
+    print_array(array, array_size);
     printf("-> ");
-
-    for(int i = 0; i < array_index; i++)
-    {
-        printf("%d ", no_duplicates_array[i]);
-    }
+    print_array(no_duplicates_array, no_duplicates_array_size);
 
     error_code = SUCCESS;
 Exit:
     return error_code;
+}
+
+void print_array(int array[], int array_size){
+    for(int i = 0; i < array_size; i++)
+    {
+        printf("%d ", array[i]);
+    }
 }
 
 bool is_element_in_array(int array[], int element, int array_size){
@@ -82,4 +73,22 @@ bool is_element_in_array(int array[], int element, int array_size){
     }
     
     return false;
+}
+
+int construct_new_array_with_no_duplicates(int array[], int array_size, int no_duplicates_array[]){
+    int current_element_to_inspect_duplicates = 0;
+    int array_index = 0;
+
+    for(int i = 0; i < array_size; i++)
+    {
+        current_element_to_inspect_duplicates = array[i];
+        if(is_element_in_array(no_duplicates_array, current_element_to_inspect_duplicates, array_index))
+        {
+            continue;
+        }
+        no_duplicates_array[array_index] = current_element_to_inspect_duplicates;
+        array_index += 1;
+    }
+
+    return array_index;
 }
