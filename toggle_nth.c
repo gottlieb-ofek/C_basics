165c6e2580431ab2f222f794baa4369972126663
gottl <gottlieb.ofek@gmail.com>
2025-06-23 11:42:50 +0300

    moved all declerations to begining

--- toggle_nth.c
+++ toggle_nth.c
@@ -5,6 +5,7 @@ int main() {
     int num = 0;
     int nth_bit  = 0;
     int num_after_toggle = 0;
+    int mask_for_nth_bit_on = 0;
 
     printf("Input number: ");
     scanf("%d", &num);
@@ -12,7 +13,7 @@ int main() {
     printf("Input nth bit to toggle: ");
     scanf("%d", &nth_bit);
     
-    int mask_for_nth_bit_on = 1 << (nth_bit - 1);
+    mask_for_nth_bit_on = 1 << (nth_bit - 1);
     num_after_toggle = num ^  mask_for_nth_bit_on;
 
     printf("After toggling nth bit: %d", num_after_toggle);
